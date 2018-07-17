900955c683ff2a21ee99a771cfc48224579bef61
pankajsaini123 <pankajsaini982134@gmail.com>
2018-07-17 11:11:36 +0530

    chat-application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import {RouterModule, Routes } from '@angular/router';
+
+import { ChatModule } from './chat/chat.module';
+import { UserModule } from './user/user.module';
+import { LoginComponent } from './user/login/login.component';
+import { CookieService } from 'ngx-cookie-service';
+
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
+    ChatModule,
+    UserModule,
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent, pathMatch: 'full'},
+      {path: '', redirectTo: 'login', pathMatch: 'full' },
+      {path: '*', component: LoginComponent },
+      {path: '**',  component: LoginComponent }
+    ])
   ],
-  providers: [],
+  providers: [CookieService,
+              HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/user/login/login.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+
+
+import { AppService } from '../../app.service';
+import { Router } from '@angular/router';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrService } from 'ngx-toastr';
+import { CookieService } from 'ngx-cookie-service';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  public email: any;
+  public password: any;
+
+  constructor(public appService: AppService, public router: Router, public toastr: ToastrService, public cookie: CookieService) {
+    console.log('login component called');
+   }
+
+ngOnInit() {
+  }
+
+
+
+  public goToSignUp: any = () => {
+    this.router.navigate(['/sign-up']);
+  } // end to sign-up
+
+  public signinFunction: any = () => {
+    if (!this.email) {
+      this.toastr.warning('enter email');
+    } else if (!this.password) {
+
+      this.toastr.warning('enter password');
+    } else {
+
+      const data = {
+        email: this.email,
+        password: this.password
+      };
+
+      this.appService.signinFunction(data).subscribe((apiResponse) => {
+       if (apiResponse.status === 200) {
+          console.log(apiResponse);
+          this.cookie.set('authtoken', apiResponse.data.authToken);
+          this.cookie.set('receiverId', apiResponse.data.userDetails.userId);
+          this.cookie.set('receiverName', apiResponse.data.userDetails.firstName + ' ' + apiResponse.data.userDetails.lastName);
+          this.appService.setUserInfoInLocalStorage(apiResponse.data.userDetails);
+          this.router.navigate(['/chat']);
+        } else {
+          this.toastr.error(apiResponse.message);
+        }
+      },
+      (err) => {
+        this.toastr.error('some error occured');
+      });
+
+    }
+  }
+}
